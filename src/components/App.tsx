4a293d89e11bccc130a35fce6af7b7074c118387
seanmcpherson <seanmcpherson@tsys.com>
2018-12-01 11:22:19 -0500

    Fetch and render books

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+interface Book {
+  title: string,
+  author: string,
+  startDate: string,
+  endDate: string,
+  genre: string,
+  comments: string,
+  rating: string,
+  wouldRecommend: boolean,
+  haveReadBefore: boolean
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    state = {
+        record: []
+    };
+    componentDidMount() {
+        fetch("./data/record.json")
+            .then((raw) => raw.json())
+            .then((res) => this.setState({ record: res }));
+    }
+    render() {
+        const { record } = this.state;
+        return (
+            <div className="App">
+                <ul className="BookList">
+                    {record.map((book: Book) => (
+                        <li className="Book" key={book.title}>
+                            {book.title} by {book.author}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
