8bbc882a51b50596680fc5d02462782e21a41bf1
Sean McPherson <seanmcp25@gmail.com>
2019-01-04 21:07:04 -0500

    Updates

---
+++ precache-manifest.558a612b06a99ee893361b010296fb41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a8d309c4da9bc73c96d",
+    "url": "/book-worm/static/js/runtime~main.4a8d309c.js"
+  },
+  {
+    "revision": "81534c3dd58d44bc2d4e",
+    "url": "/book-worm/static/js/main.81534c3d.chunk.js"
+  },
+  {
+    "revision": "e3d83c5aad416cef1f69",
+    "url": "/book-worm/static/js/1.e3d83c5a.chunk.js"
+  },
+  {
+    "revision": "81534c3dd58d44bc2d4e",
+    "url": "/book-worm/static/css/main.82c44a71.chunk.css"
+  },
+  {
+    "revision": "e7b0c58b2b630c3655b3803ef0d8312c",
+    "url": "/book-worm/index.html"
+  }
+];
